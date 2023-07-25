425fe45ad6e577d9872c6e6c6031bf25301d52e2
Tarik <tarikozgur0607@gmail.com>
2023-07-25 13:16:50 +0300

    add new codes

--- src/index.js
+++ src/index.js
@@ -2,64 +2,124 @@ import AboutMe from "./about";
 import contactMe from "./contact";
 import contact from "./contact";
 
+function component() {
+  const element = document.createElement("div");
+  const topNav = document.createElement("div");
+  const para = document.createElement("p");
 
-function component( ){
-    const element = document.createElement("div");
-    const topNav = document.createElement("div");
-    const para = document.createElement("p");
-    const about = document.createElement("a");
-    const main = document.createElement("a");
-    const contact = document.createElement("a");
-    about.innerText = "About";
-    main.innerText = "Main";
-    contact.innerText ="contact";
-   para.innerText  ="An easy and delicious Chicken Kebab recipe. This recipe includes a tasty yoghurt marinade. These chicken kebab skewers are so versatile, they can be grilled, baked in the oven or cooked on the BBQ. A new healthy version of your favourite Greek takeaway.   "
-    const header = document.createElement("h1");
-    header.innerText = "Chicken Kebab"
-    //Element Style
-    element.style.display = "flex";
-    element.style.flexDirection = "column";
-    element.style.justifyContent = "center";
-    element.style.alignItems ="center";
-    element.style.margin = "auto";
-    //Img
-    const MyImg = new Image(500,300)
-    MyImg.src = ("../dist/chicken.jpg")
-    //button
-    about.addEventListener("click",()=>{
-        header.innerText ="About Page";
-        MyImg.src = ("../dist/about.jpg");
-                            
-    });
-    contact.addEventListener("click",()=>{
-        header.innerText = "Contact Page";
-        MyImg.src = ("../dist/contact.jpg");
+  const about = document.createElement("a");
+  const main = document.createElement("a");
+  const contact = document.createElement("a");
 
-    })
+  //box
+  const boxContainer = document.createElement("div");
+  const box1 = document.createElement("div");
+  const box2 = document.createElement("div");
+  const box3 = document.createElement("div");
 
-    //button events
-    // btn.addEventListener("click",()=>{
+  about.innerText = "About";
+  main.innerText = "Main";
+  contact.innerText = "contact";
+  para.innerText = "Main";
+  para.style.textAlign = "center";
+  
+  const header = document.createElement("h1");
+  header.innerText = "Chicken Kebab";
+  //box
+  boxContainer.style.display = "flex";
+  boxContainer.style.gap = "50px";
+  boxContainer.style.background = "white";
+  boxContainer.style.padding = "30px";
 
-    // })
-    // btnC.addEventListener("click",()=>{
+  //box1
+  box1.style.width = "200px";
+  box1.style.height = "100px";
+  box1.style.background = "purple";
+  box1.style.color = "white";
+  box1.innerText ="Chicken Kebab";
+  //box2
+  box2.style.width = "200px";
+  box2.style.height = "100px";
+  box2.style.background = "purple";
+  box2.style.color = "white";
+  box2.innerText ="Delicious Food";
 
-    // })
+  //box3
+  box3.style.width = "200px";
+  box3.style.height = "100px";
+  box3.style.background = "purple";
+  box3.style.color = "white";
+  box3.innerText ="Sweet";
 
-    //top navs
-    topNav.style.backgroundColor = "blue";
-    topNav.style.display = "flex";
-    topNav.style.flexDirection = "row";
-    topNav.style.gap = "50px";
-    
-    topNav.appendChild(about)
-    topNav.appendChild(contact)
-        //element append childs
-    element.appendChild(topNav)
-    element.appendChild(header)
-    element.appendChild(MyImg)
-    element.appendChild(para)
+  //Element Style
+  element.style.display = "flex";
+  element.style.flexDirection = "column";
+  element.style.justifyContent = "center";
+  element.style.alignItems = "center";
+  element.style.margin = "auto";
+  //Img
+  const MyImg = new Image(500, 300);
+  MyImg.src = "../dist/chicken.jpg";
+  //button
+  about.addEventListener("click", () => {
+    header.innerText = "About Page";
+    MyImg.src = "../dist/about.jpg";
+    box1.innerText = "About Page this chicken foods";
+    box2.innerText  ="this brand a good brand";
+    box3.innerText =  "hey i love chicken";
+    para.innerText = "About";
+});
+  contact.addEventListener("click", () => {
+    header.innerText = "Contact Page";
+    MyImg.src = "../dist/contact.jpg";
+    box1.innerText = "E mail : notrealmail@gmail.com ";
+    box2.innerText  ="Phone number : XXX 000 XXX";
+    box3.innerText =  "Adress : World";
+    para.innerText = "Contact";
+  });
+  main.addEventListener("click", () => {
+    MyImg.src = "../dist/chicken.jpg";
+ 
+    header.innerText = "Chicken Kebab";
+    box1.innerText ="Chicken Kebab";
+    box2.innerText ="Delicious Food";
+    box3.innerText ="Sweet";
 
 
-return element
+
+  });
+  //default box styles
+ 
+  //button events
+  // btn.addEventListener("click",()=>{
+
+  // })
+  // btnC.addEventListener("click",()=>{
+
+  // })
+
+  //top navs
+  topNav.style.backgroundColor = "black";
+  topNav.style.color = "white";
+  topNav.style.display = "flex";
+  topNav.style.flexDirection = "row";
+  topNav.style.gap = "50px";
+  topNav.appendChild(main);
+  topNav.appendChild(about);
+  topNav.appendChild(contact);
+
+  //box container
+  //element append childs
+  boxContainer.appendChild(box1);
+  boxContainer.appendChild(box2);
+  boxContainer.appendChild(box3);
+
+  element.appendChild(topNav);
+  element.appendChild(header);
+  element.appendChild(MyImg);
+  element.appendChild(para);
+  element.appendChild(boxContainer);
+
+  return element;
 }
- document.body.appendChild(component());
+document.body.appendChild(component());
