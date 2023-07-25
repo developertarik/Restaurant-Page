818abc44ce59329ccad1d1e1f81f34dfb990c7a5
Tarik <tarikozgur0607@gmail.com>
2023-07-25 12:43:03 +0300

    add new codes

--- src/index.js
+++ src/index.js
@@ -3,17 +3,44 @@ import contactMe from "./contact";
 import contact from "./contact";
 
 
-function component(){
+function component( ){
     const element = document.createElement("div");
-    const btn = document.createElement("button");
-    const btnC  = document.createElement("button")
-    element.innerHTML = "This developer a good developer bro";
-    btnC.innerText = "Contact "
-    btn.innerHTML = "About";
-btn.onclick = AboutMe;
-btnC.onclick = contactMe;
-element.appendChild(btn)
-element.appendChild(btnC)
+    const topNav = document.createElement("div");
+    const about = document.createElement("a");
+    const contact = document.createElement("a");
+    about.innerText = "about";
+   
+    const header = document.createElement("h1");
+    header.innerText = "Chicken Kebab"
+    //Element Style
+    element.style.display = "flex";
+    element.style.flexDirection = "column";
+    element.style.justifyContent = "center";
+    element.style.alignItems ="center";
+    element.style.margin = "auto";
+    //Img
+    const MyImg = new Image(100,200)
+    MyImg.src = ("../dist/chicken.jpg")
+    //button
+    about.addEventListener("click",()=>{
+        header.innerText ="About Page";
+                            
+    });
+
+    //button events
+    // btn.addEventListener("click",()=>{
+
+    // })
+    // btnC.addEventListener("click",()=>{
+
+    // })
+    //element append childs
+    topNav.appendChild(about)
+    topNav.appendChild(contact)
+    element.appendChild(topNav)
+    element.appendChild(header)
+    element.appendChild(MyImg)
+
 return element
 }
-document.body.appendChild(component());
+ document.body.appendChild(component());
